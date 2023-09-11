480c8b6d40817b0a841d3bd4b4f80d5ff66d4a52
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-11 16:19:01 -0700

    Updated src-sw

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,15 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+//From week 19
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ]
+  })
+);
