4c91e3d463fdadb0b09749b5d7d5e92a9a622020
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-11 16:54:37 -0700

    Got rid of TODOs

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
@@ -26,7 +25,6 @@ export const putDb = async (content) => {
   await tx.done;
 }
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await openDB('jate', 1); 
   const tx = db.transaction('jate', 'readonly');
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,14 +1,12 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
   window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
   butInstall.style.display = 'none';
   window.deferredPrompt.prompt();
@@ -21,7 +19,6 @@ butInstall.addEventListener('click', async () => {
   window.deferredPrompt = null;
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log("INstalled")
 });
